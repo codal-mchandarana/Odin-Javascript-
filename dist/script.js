d47a74da8ff70ef4f420ee61d0fac91128211924
codal-mchandarana <mchandarana@codal.com>
2024-10-26 02:00:10 +0530

    CIT-491 #time 2h Completed the frontend of tic tac toe game

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,4 @@
+////////////////////// Setting, Reseting and Getting value of tic tac toe game
 const Gameboard = () => {
   const rows = 3;
   const columns = 3;
@@ -14,8 +15,14 @@ const Gameboard = () => {
   };
 
   ///// Will return the board
-  const getBoard = () => {
-    return board;
+  const getBoard = () => board;
+
+  ///// Marking board
+  const markBoard = (row, column, playermark) => {
+    let currentValue = board[row][column].getValue();
+    if (currentValue !== "") return false;
+    board[row][column].mark(playermark);
+    return true;
   };
 
   ///// For creating each cell of board
@@ -41,31 +48,377 @@ const Gameboard = () => {
 
   resetBoard();
 
-  return { getBoard, resetBoard };
+  return { getBoard, markBoard, resetBoard };
 };
 
-const boardDiv = document.querySelector(".board");
-const infoBarDiv = document.querySelector(".info-bar");
-const updateScreen = () => {
-  const game = Gameboard();
-  boardDiv.textContent = "";
-  const board = game.getBoard();
+//////////////////// Creating player , handling turn and logic of whole game
+const GameController = () => {
+  const board = Gameboard();
+  let winLine = {};
+  let players;
+  let turnCounter;
+  let gameActiveState = false;
 
-  if (Info.getInfo() === "") {
-    Info.setInfo("Welcome! check the buttons below");
-  }
-  console.log("Info bar here ",infoBarDiv)
-  infoBarDiv.textContent = Info.getInfo();
+  ////////// Part-1 Player
+  const Players = () => {
+    let players = [
+      {
+        name: "Player One",
+        mark: "O",
+        score: 0,
+        index: 0,
+      },
+      {
+        name: "Player Two",
+        mark: "X",
+        score: 0,
+        index: 1,
+      },
+    ];
 
-  board.forEach((row, index1) => {
-    row.forEach((cell, index2) => {
-      const cellButton = document.createElement("button");
+    let startPlayer = Math.round(Math.random());
+    let activePlayer = players[startPlayer];
 
-      cellButton.classList.add("bg-boardCellBg", "rounded-[8px]");
+    ////// Getting player info
 
-      boardDiv.appendChild(cellButton);
+    const getActivePlayer = () => activePlayer;
+
+    const getPlayers = () => players;
+
+    /////// Setting Player info
+
+    const setPlayerNames = (playerOneName, playerTwoName) => {
+      players[0].name = playerOneName == "" ? "Player One" : playerOneName;
+      players[1].name = playerTwoName == "" ? "Player Two" : playerTwoName;
+    };
+
+    const setPlayerMarks = (playerOneMark, playerTwoMark) => {
+      players[0].mark = playerOneMark;
+      players[1].mark = playerTwoMark;
+    };
+
+    ////// Switch Players turn
+
+    const switchStartPlayer = () => {
+      startPlayer = startPlayer === players[0] ? players[1] : players[0];
+      activePlayer = startPlayer;
+    };
+
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    /////// Modifying players score
+
+    const incrementScores = (playerIndex) => {
+      players[playerIndex].score++;
+    };
+    const resetScores = () => {
+      players[0].score = 0;
+      players[1].score = 0;
+    };
+
+    return {
+      getActivePlayer,
+      getPlayers,
+      setPlayerMarks,
+      setPlayerNames,
+      switchPlayerTurn,
+      switchStartPlayer,
+      incrementScores,
+      resetScores,
+    };
+  };
+  players = Players();
+
+  ////////// Part-2 Player Turn
+  const TurnCounter = () => {
+    let turnNumber = 1;
+
+    const turn = () => turnNumber;
+    const increment = () => turnNumber++;
+    const reset = () => (turnNumber = 1);
+
+    return {
+      turn,
+      increment,
+      reset,
+    };
+  };
+  turnCounter = TurnCounter();
+
+  ///////// Part-3 Setting and getting winline
+  const setWinLine = (start, end) => {
+    winLine = { start, end };
+  };
+
+  const getWinLine = () => winLine;
+
+  ///////// Part-4 Setting and getting Game State
+
+  const toggleGameActiveState = () => {
+    gameActiveState = gameActiveState ? false : true;
+  };
+
+  const getGameActiveState = () => gameActiveState;
+
+  ///////// Part-5  Winning and results
+
+  const checkForWin = () => {
+    const checkLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const flatBoard = board
+      .getBoard()
+      .map((row) => row.map((cell) => cell.getValue()))
+      .flat(1);
+
+    for (const checkLine of checkLines) {
+      const a = flatBoard[checkLine[0]];
+      const b = flatBoard[checkLine[1]];
+      const c = flatBoard[checkLine[2]];
+
+      const match = a === b && b === c && c != "";
+      if (match) {
+        return { gameWon: true, start: checkLine[0], end: checkLine[2] };
+      }
+    }
+    return { gameWon: false };
+  };
+
+  const resetGame = () => {
+    winLine = {};
+    turnCounter.reset();
+    board.resetBoard();
+    players.switchStartPlayer();
+
+    Info.setInfo(`Game begins ${players.getActivePlayer().name}'s turn...`);
+  };
+
+  const playRound = (row, column) => {
+    const validMove = board.markBoard(
+      row,
+      column,
+      players.getActivePlayer().mark
+    );
+    if (!validMove) return;
+
+    const winCheck = checkForWin();
+    if (winCheck.gameWon) {
+      Info.setInfo(`A win! 1 point to ${players.getActivePlayer().name}!`);
+      setWinLine(winCheck.start, winCheck.end);
+      players.incrementScores(players.getActivePlayer().index);
+      toggleGameActiveState();
+      return;
+    }
+    if (turnCounter.turn() === 9) {
+      Info.setInfo("Draw game!");
+      toggleGameActiveState();
+      return;
+    }
+
+    //// Next Round
+    turnCounter.increment();
+    players.switchPlayerTurn();
+    Info.setInfo(`${players.getActivePlayer().name}'s turn...`);
+  };
+
+  return {
+    setPlayerNames: players.setPlayerNames,
+    setPlayerMarks: players.setPlayerMarks,
+    incrementScores: players.incrementScores,
+    resetScores: players.resetScores,
+    getPlayers: players.getPlayers,
+    playRound,
+    getBoard: board.getBoard,
+    getWinLine,
+    resetGame,
+    toggleGameActiveState,
+    getGameActiveState,
+  };
+};
+
+//////////////////// Managing whole screen.
+const ScreenController = () => {
+  const game = GameController();
+  const boardDiv = document.querySelector(".board");
+  const infoBarDiv = document.querySelector(".info-bar");
+  const toolbar = document.querySelector(".toolbar");
+  const toolbarBtnArr = Array.from(document.querySelector(".toolbar").children);
+  const markGroups = document.querySelectorAll(".mark-group");
+  const scores = document.querySelectorAll(".score");
+
+  // Updating screen when first time screen is loaded or when any action is performed on a particular cell
+  const updateScreen = () => {
+    boardDiv.textContent = "";
+    const board = game.getBoard();
+    const winLineData = game.getWinLine();
+
+    if (Info.getInfo() === "") {
+      Info.setInfo("Welcome! check the buttons below");
+    }
+    infoBarDiv.textContent = Info.getInfo();
+
+    board.forEach((row, index1) => {
+      row.forEach((cell, index2) => {
+        const cellButton = document.createElement("button");
+
+        cellButton.classList.add("bg-boardCellBg", "rounded-[6px]","text-7xl");
+        cellButton.dataset.row = index1;
+        cellButton.dataset.column = index2;
+        let cellValue = cell.getValue();
+        cellButton.textContent = cellValue;
+        if (cellValue === "O") {
+          cellButton.classList.add("nought");
+        }
+        if (cellValue === "X") {
+          cellButton.classList.add("cross");
+        }
+
+        boardDiv.appendChild(cellButton);
+      });
     });
-  });
+
+    if (Object.keys(winLineData).length != 0) {
+      toggleToolbar(true, true, true);
+
+      scores[0].innerText = game.getPlayers()[0].score;
+      scores[1].innerText = game.getPlayers()[1].score;
+    }
+
+    if (infoBarDiv.textContent === "Draw game!") {
+      toggleToolbar(true, true, true);
+    }
+  };
+
+  const toggleToolbar = (b0, b1, b2) => {
+    toolbarBtnArr[0].disabled = !b0;
+    toolbarBtnArr[1].disabled = !b1;
+    toolbarBtnArr[2].disabled = !b2;
+  };
+
+  const togglePlayerInfo = (btn) => {
+    let nameInputs = document.querySelectorAll(".name-input");
+    nameInputs.forEach((nameInput) => {
+      nameInput.disabled = nameInput.disabled ? false : true;
+    });
+
+    array1 = Array.from(markGroups[0].childNodes).concat(
+      Array.from(markGroups[1].childNodes)
+    );
+    array1.forEach((markGroups) => {
+      markGroups.disabled === true
+        ? (markGroups.disabled = false)
+        : (markGroups.disabled = true);
+    });
+
+    if (nameInputs[0].disabled === false) {
+      nameInputs[0].focus();
+      toggleToolbar(true, false, false);
+      btn.innerText = "Done";
+      Info.setInfo("Enter you names and choose Marks");
+    } else {
+      toggleToolbar(true, true, true);
+      btn.innerText = "Set Players";
+      game.setPlayerNames(nameInputs[0].value, nameInputs[1].value);
+      Info.setInfo("Names Updated.  Ready for a New Game?");
+    }
+  };
+
+  // Add event listener for the board cell
+  const clickHandlerBoard = (e) => {
+    if (!game.getGameActiveState()) return;
+
+    const selectedRow = e.target.dataset.row;
+    const selectedColumn = e.target.dataset.column;
+
+    if (!selectedRow || !selectedColumn) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+  };
+
+  // Add event listener for the toolbar
+  const clickHandlerToolbar = (e) => {
+    switch (parseFloat(e.target.dataset.groupPosition)) {
+      case 1:
+        togglePlayerInfo(e.target);
+        infoBarDiv.textContent = Info.getInfo();
+        break;
+
+      case 2:
+        toggleToolbar(false, false, false);
+        game.toggleGameActiveState();
+        game.resetGame();
+        updateScreen();
+        break;
+
+      case 3:
+        scores[0].innerText = 0;
+        scores[1].innerText = 0;
+        game.resetScores();
+        Info.setInfo("Scores reset.  Ready for a New Game?");
+        infoBarDiv.textContent = Info.getInfo();
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const clickHandlerMarkGroup = (e) => {
+    const updateMarksClass = (on1, on2, off1, off2) => {
+      let marksArr = Array.from(markGroups[0].children).concat(
+        Array.from(markGroups[1].children)
+      );
+      if (marksArr[on1].classList.contains("off")) {
+        marksArr[on1].classList.remove("off");
+      }
+      if (marksArr[on2].classList.contains("off")) {
+        marksArr[on2].classList.remove("off");
+      }
+      marksArr[off1].classList.add("off");
+      marksArr[off2].classList.add("off");
+    };
+
+    const markIndex = e.target.dataset.markIndex;
+
+    switch (markIndex) {
+      case "0":
+        game.setPlayerMarks("O", "X");
+        updateMarksClass(0, 3, 1, 2);
+        break;
+      case "1":
+        game.setPlayerMarks("X", "O");
+        updateMarksClass(1, 2, 0, 3);
+        break;
+      case "2":
+        game.setPlayerMarks("X", "O");
+        updateMarksClass(1, 2, 0, 3);
+        break;
+      case "3":
+        game.setPlayerMarks("O", "X");
+        updateMarksClass(0, 3, 1, 2);
+        break;
+      default:
+        log("huh");
+        break;
+    }
+  };
+
+  boardDiv.addEventListener("click", clickHandlerBoard);
+  toolbar.addEventListener("click", clickHandlerToolbar);
+  markGroups[0].addEventListener("click", clickHandlerMarkGroup);
+  markGroups[1].addEventListener("click", clickHandlerMarkGroup);
+  updateScreen();
 };
 
 ///// Will give the info above the screen
@@ -84,4 +437,4 @@ const Info = (() => {
   };
 })();
 
-updateScreen();
+ScreenController();
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         playerNametext: "rgb(185, 28, 0)",
         playerNameBorder: "hsl(46, 80%, 63%)",
         InputBorderColor: "hsl(44, 16%, 77%)",
+        InputEnableBorderColor:"hsl(54, 100%, 50%)",
         InputBg: "hsl(51, 14%, 86%)",
         mark0: "hsl(10, 90%, 55%)",
         markx: "hsl(216, 90%, 55%)",
@@ -26,16 +27,25 @@ module.exports = {
         toolbarBorder: "hsl(46, 100%, 40%)",
         toolbarBg: "hsl(46, 100%, 50%)",
         toolbarTextColor: "rgb(185, 28, 0)",
+        toolbarTextDisColor:"rgb(46, 46, 46)",
 
         boardCellBg: "hsl(52, 100%, 96%)",
 
         deactiveColor: "rgb(156, 156, 156)",
+
+        nought:"hsl(10, 90%, 55%)",
+        cross:"hsl(216, 90%, 55%)",
+
+        white:"hsl(0, 0%, 100%)",
+        disableColor:"rgba(16, 16, 16, 0.3)",
+        disableBorder :"rgba(118, 118, 118, 0.3)"
       },
       fontSize: {
         "3xl": "1.6rem",
         "3.5xl": "1.8rem",
         "4xl": "2rem",
         "5xl": "2.75rem",
+        "7xl":"7rem",
       },
       fontWeight: {
         customBold: "800",
